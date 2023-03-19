5cbf72f49b68feb47cf58d4081c027b84c08f43d
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-03-19 23:08:28 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,13 +5,13 @@ def build_heap(data):
     a = len(data)
     swaps = []
     for i in range(a, -1, -1):
-        j=i
+        j = i
         while True:
             lc = j * 2 + 1
             if lc >= a:
                 break
             if lc + 1 < a and data[lc+1] < data[lc]:
-                lc = lc + 1
+                lc += 1
             if data[j] < data[lc]:              
                 swaps.append((j, lc))
                 data[j], data[lc] = data[lc] , data[j]
@@ -19,17 +19,11 @@ def build_heap(data):
             else:
                 break
     return swaps                
-                
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
- 
-    
-
+  
 
 def main():
-    option = str(input())
-    if "I" in option:
+    option = input()
+    if option == 'I':
         n = int(input())
         data = list(map(int, input().split()))
         assert len(data) == n
@@ -37,17 +31,16 @@ def main():
         print(len(swaps))
         for i, j in swaps:
             print(i, j)
-        return
-    if "F" in option:
-        file_name = str(input())
-        file_name = "tests/" + str(file_name)
+    elif option == 'F':
+        file_name = input().strip()
         with open(file_name, 'r') as file:
             n = int(file.readline())
             data = list(map(int, file.readline().split()))
-        assert len(data) == n            
+        assert len(data) == n
         swaps = build_heap(data)
         print(len(swaps))
-        return    
+    else:
+        print("Invalid option")
   
 
 
